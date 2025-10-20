f03483855570cca3ccb376a79280e2204180821a
tejaswi <potutejaswireddy@gmail.com>
2025-10-20 14:46:15 -0400

    first commit

---
+++ demo2/src/main/java/io/javabrains/demo2/model/User.java
@@ -0,0 +1,24 @@
+package io.javabrains.demo2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users2")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
---
+++ demo2/src/main/java/io/javabrains/demo2/service/UserService.java
@@ -0,0 +1,45 @@
+// src/main/java/io/javabrains/demo2/service/UserService.java
+package io.javabrains.demo2.service;
+
+import io.javabrains.demo2.model.User;
+import io.javabrains.demo2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> update(Long id, User userDetails) {
+        return userRepository.findById(id).map(user -> {
+            user.setName(userDetails.getName());
+            user.setEmail(userDetails.getEmail());
+            return userRepository.save(user);
+        });
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
